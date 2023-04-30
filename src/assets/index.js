e9c7e23d04a8d512613651ddf40f03c7ec6ce9d3
Jayanika Chandrapriya <contact@jayanika.com>
2023-05-01 02:24:11 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,95 @@
+import airbnb from "./airbnb.svg";
+import airbnb_2 from "./airbnb-2.svg";
+import android from "./android.svg";
+import apple from "./apple.svg";
+
+import arrow_right from "./arrow-right.svg";
+import automatic from "./automatic.svg";
+import bissell from "./bissell.svg";
+import bissell_white from "./bissell-white.svg";
+
+import briefcase from "./briefcase.svg";
+import check_circle from "./check-circle.svg";
+import company_logos_cover from "./company-logos-cover.webp";
+import coty from "./coty.svg";
+
+import doller_circle from "./doller-circle.svg";
+import dropdown from "./dropdown.svg";
+import enterprise from "./enterprise.png";
+import facebook from "./facebook.svg";
+
+import favicon from "./favicon.ico";
+import find_great_work from "./find-great-work.jpg";
+import find_talent from "./find-talent.webp";
+import godaddy_black from "./godaddy-black.svg";
+
+import go_daddy_white from "./go-daddy-white.svg";
+import group from "./group.svg";
+import image_1 from "./image-1.jpg";
+import instagram from "./instagram.svg";
+
+import linkedin from "./linkedin.svg";
+import logo from "./logo.svg";
+import microsoft from "./microsoft.svg";
+import microsoft_white from "./microsoft-white.svg";
+
+import up_your_work from "./up-your-work.gif";
+
+import nasdaq from "./nasdaq.svg";
+import rancher from "./rancher.svg";
+import reading from "./reading.png";
+import right_arrow from "./right-arrow.svg";
+
+import search from "./search.svg";
+import skills from "./skills.svg";
+import star from "./star.svg";
+import star_circle from "./star-circle.svg";
+
+import start_white from "./start-white.svg";
+import support from "./support.svg";
+import twitter from "./twitter.svg";
+import youtube from "./youtube.svg";
+
+export {
+    airbnb ,
+    airbnb_2 ,
+    android ,
+    apple ,
+    arrow_right ,
+    automatic ,
+    bissell ,
+    bissell_white ,
+    briefcase ,
+    check_circle ,
+    company_logos_cover ,
+    coty ,
+    doller_circle ,
+    dropdown ,
+    enterprise ,
+    facebook ,
+    favicon ,
+    find_great_work ,
+    find_talent ,
+    godaddy_black ,
+    go_daddy_white ,
+    group ,
+    image_1 ,
+    instagram ,
+    linkedin ,
+    logo ,
+    microsoft ,
+    microsoft_white ,
+    nasdaq ,
+    rancher ,
+    reading ,
+    right_arrow ,
+    search ,
+    skills ,
+    star ,
+    star_circle ,
+    start_white ,
+    support ,
+    twitter ,
+    youtube,
+    up_your_work
+}
